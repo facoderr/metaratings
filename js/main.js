6f020216201d0f0082d3c26e6965f297612aafff
Farkhad Rizvanov <farkhad-r@mail.ru>
2019-08-10 04:03:03 +0300

    Added project files

---
+++ js/main.js
@@ -0,0 +1,80 @@
+$(document).ready(function() {
+
+	//  Event
+
+	$('html').swipe({
+		swipeRight: function(event, direction, distance, duration, fingerCount) {
+			$('.js-toggle').addClass('is-active');
+			$('.nav').addClass('is-open');
+		},
+		swipeLeft: function(event, direction, distance, duration, fingerCount) {
+			var heightChild = $('.nav-menu').find('> li').outerHeight();
+				countChild = $('.nav-menu').find('> li').length;
+				totalHeight = heightChild * countChild;
+			$('.nav-wrap').css('min-height', totalHeight);
+			$('.nav-head').removeClass('is-active');
+			$('.nav-menu').removeClass('is-out');
+			$('.nav-submenu').removeClass('is-out').removeClass('is-active').addClass('is-hidden');
+			$('.js-toggle').removeClass('is-active');
+			$('.nav').removeClass('is-open');
+		},
+		threshold: 25
+	});
+
+	//
+
+	// Click Event
+
+	$(document).on('click', '.js-toggle', function() {
+		$(this).addClass('is-active');
+		$('.nav').addClass('is-open');
+	});
+	$(document).on('click', '.js-nav-close', function() {
+		var heightChild = $('.nav-menu').find('> li').outerHeight();
+				countChild = $('.nav-menu').find('> li').length;
+				totalHeight = heightChild * countChild;
+		$('.nav-wrap').css('min-height', totalHeight);
+		$('.nav-head').removeClass('is-active');
+		$('.nav-menu').removeClass('is-out');
+		$('.nav-submenu').removeClass('is-out').removeClass('is-active').addClass('is-hidden');
+		$('.js-toggle').removeClass('is-active');
+		$('.nav').removeClass('is-open');
+	});
+	$('.has-child').children('.js-child').on('click', function() { 
+		var selected = $(this);
+				heightChild = selected.next('ul').find('> li').outerHeight();
+				countChild = selected.next('ul').find('> li').length;
+				totalHeight = heightChild * countChild;
+		$('.nav-wrap').css('min-height', totalHeight);
+		selected.next('ul').removeClass('is-hidden').addClass('is-active').end().parent('.has-child').parent('ul').addClass('is-out');
+		$('.nav-head').toggleClass('is-active', $('.nav-menu').hasClass('is-out'));
+	});
+	$(document).on('click', '.js-back', function() {
+		var selected = $(this);
+				heightChild = selected.parent().parent().parent().parent().find('> li').outerHeight();
+				countChild = selected.parent().parent().parent().parent().find('> li').length;
+				totalHeight = heightChild * countChild;
+		$('.nav-wrap').css('min-height', totalHeight);
+		selected.parent().parent('ul').removeClass('is-active').addClass('is-hidden').parent('.has-child').parent('ul').removeClass('is-out');
+		$('.nav-head').toggleClass('is-active', $('.nav-menu').hasClass('is-out'));
+	});
+
+	//
+
+
+	// Load Event
+
+	$(window).on('load', function() {
+		$('.js-logo polygon').css('fill-opacity', '1');
+		$('.js-logo').fadeOut('slow');
+		$('.js-preloader').delay(400).fadeOut('slow');
+		$('.navbar').addClass('is-animate');
+		var heightChild = $('.nav-menu').find('> li').outerHeight();
+				countChild = $('.nav-menu').find('> li').length;
+				totalHeight = heightChild * countChild;
+		$('.nav-wrap').css('min-height', totalHeight);
+	});
+
+	//
+	
+});
