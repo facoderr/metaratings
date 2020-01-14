f26294b77a35d5c9e15235ada81614a7adc8b3ee
Farkhad Rizvanov <farkhad-r@mail.ru>
2020-01-14 15:34:38 +0500

    Fix bugs

--- js/rating-bk.js
+++ js/rating-bk.js
@@ -1,27 +1,40 @@
 $(document).ready(function() {
 
 	// Rating Event
+	let overflow = $('html, body'),
+		swipeItem = $('.js-rating-item');
 
-	$('.js-rating-item').swipe({
+	swipeItem.swipe({
 		swipeStatus: function(event, phase, direction, distance, duration, fingerCount, fingerData, currentDirection) {
 			let $this = $(this);
+			if (phase == 'move') {
+				if (direction == 'left') {
+					overflow.css('overflow', 'hidden');
+				}
+				if (direction == 'right') {
+					overflow.css('overflow', 'hidden');
+				}
+			}
 			if (phase == 'end') {
 				if (direction == 'left') {
 					$this.addClass('is-swipe is-swiping');
-					setTimeout(function() {
+					setTimeout(function () {
+						overflow.css('overflow', 'hidden auto');
 						$this.removeClass('is-swiping');
 					}, 300);
 				}
 				if (direction == 'right') {
 					$this.addClass('is-swiping');
 					$this.removeClass('is-swipe');
-					setTimeout(function() {
+					setTimeout(function () {
+						overflow.css('overflow', 'hidden auto');
 						$this.removeClass('is-swiping');
 					}, 300);
 				}
 			}
 		},
 		allowPageScroll: 'none',
+		preventDefaultEvents: false,
 		triggerOnTouchEnd: false,
 		threshold: 20
 	});
