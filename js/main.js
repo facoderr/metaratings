d3c22f5a4d8c194f9e2fa7b4cf4f32271324adef
Farkhad Rizvanov <farkhad-r@mail.ru>
2020-01-30 03:04:04 +0500

    Revert "Test scroll"
    This reverts commit 1c7b12575a3a6158ffaff6ea427c74d1e9a83006.

--- js/main.js
+++ js/main.js
@@ -20590,7 +20590,7 @@ $(function () {
       modal = $('.js-modal'),
       modalVisible = $('.js-modal-visible'),
       modalWrap = $('.js-modal-wrap'),
-      modalTarget = document.querySelector('.js-review-body'),
+      modalTarget = document.querySelector('.sentiment-review-modal'),
       modalShow = '.js-modal-show',
       modalHide = '.js-modal-hide',
       modalTag;
@@ -20603,7 +20603,17 @@ $(function () {
     }
 
     html.css('overflow', 'initial');
-    disableBodyScroll(modalTarget);
+    disableBodyScroll(modalTarget, {
+      allowTouchMove: function allowTouchMove(el) {
+        while (el && el !== document.body) {
+          if (el.getElementsByClassName('js-review-body') !== null) {
+            return true;
+          }
+
+          el = el.parentNode;
+        }
+      }
+    });
     $('.js-modal-visible' + modalTag).addClass('is-open');
     $('.js-modal' + modalTag).fadeIn().addClass('is-open');
     doc.on('mouseup touchend', function (e) {
